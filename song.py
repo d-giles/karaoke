df0bf44b69547ae26bb9f526e4748b409fa67012
Daniel <dgiles1@hawk.iit.edu>
2019-03-29 15:25:18 -0400

    Adding skeleton files, sb.ipynb is a sandbox

---
+++ song.py
@@ -0,0 +1,9 @@
+class song():
+    def __init__(self,name):
+        self.name=name
+        pass
+    def lyrics(self):
+        pass
+    def play(self,lyrics=False):
+        pass
+        # Play the song audio.
